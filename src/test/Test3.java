a1e99e90cbc01fb77152941cec2679f25753cca8
A U Thor <mateiraduconstantin@github.com>
2019-07-13 12:20:17 +0300

    Add constructer to Horia

--- src/test/Test3.java
+++ src/test/Test3.java
@@ -3,5 +3,6 @@ package test;
 public class Test3 {
 	public Test3() {
 		//asd
+		
 	}
 }
--- src/test/Test4.java
+++ src/test/Test4.java
@@ -1,5 +1,8 @@
 package test;
 
 public class Test4 {
-
+	int a;
+	public Test4(int a) {
+		this.a = a;
+	}
 }
